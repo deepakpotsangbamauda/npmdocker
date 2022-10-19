deead22a65aa0efde3d76c8afad179364b2a036c
deepakpotsangbamauda <deepak.potasangbam@auda.org.au>
2022-10-19 13:14:16 +1100

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require ('express')
 const app = express()
 
-app.get('/',(req, res) =>res.send('Hello World'))
-app.listen (3000, () => console.log('server ready'))
+app.get('/',(req, res) =>res.send('Hello World how vare you today'))
+app.listen (3000, () => console.log('server ready note'))
